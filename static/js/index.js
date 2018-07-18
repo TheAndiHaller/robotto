f0785261a7a832327196fd3c93c1ab0901683cfd
IvanC0110 <41236313+IvanC0110@users.noreply.github.com>
2018-07-18 11:16:58 -0300

    asfaddsa

---
+++ static/js/index.js
@@ -0,0 +1,19 @@
+$(function(){
+    var isTouchDevice = "ontouchstart" in document.documentElement ? true : false;
+    var BUTTON_DOWN   = isTouchDevice ? "touchstart" : "mousedown";
+    var BUTTON_UP     = isTouchDevice ? "touchend"   : "mouseup";
+                
+    $("button").bind(BUTTON_DOWN,function(){
+            $.post("/control",
+            {boton: this.id},
+            function(data,status){
+              });
+            });
+    
+            $("button").bind(BUTTON_UP,function(){
+            $.post("/control",
+            {boton: "alto"},
+            function(data,status){
+              });
+            });
+    });
