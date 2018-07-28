91eebf2ffba4b57708fc1c03556c9e6c75ff6b65
IvanC0110 <41236313+IvanC0110@users.noreply.github.com>
2018-07-28 14:42:34 -0300

    Correciones ortograficas post "Todo ok"
    Correciones luego de la cagadota que se mando andi

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def command():
     return "OK"        
 
 if __name__ == "__main__":
-    app.run(host='192.168.0.102', port=5000)
+    app.run(host='192.168.1.89', port=5000)
 
 
 #127.0.0.1 (ip home)
