44e6dc074f41dd26fda81cb1d0dee358172c7415
IvanC0110 <41236313+IvanC0110@users.noreply.github.com>
2018-07-28 12:58:50 -0300

    asda

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def command():
     return "OK"        
 
 if __name__ == "__main__":
-    app.run(host='192.168.0.102', port=5000)
+    app.run(host='127.0.0.1 ', port=5000)
 
 
 #127.0.0.1 (ip home)
