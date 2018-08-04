454e38f8098e71e66e8bc6bc65fde27e78e85dc7
Andreas Haller <andi_haller@hotmail.com>
2018-08-04 13:28:50 -0300

    SerialOk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import serial
+ser = serial.Serial('COM26', 115200, timeout=0)  # open serial port Windows
 
 app = Flask(__name__)
 
@@ -19,14 +21,19 @@ def command():
     if request.method == 'POST':       
         if request.form.get("boton") == "adelante":
             print("adelante")
+            ser.write(b'f \n')     # write a string
         elif request.form.get("boton") == "atras":
             print("atras")
+            ser.write(b'b \n')     # write a string
         elif request.form.get("boton") == "izquierda":
             print("izquierda")
+            ser.write(b'l \n')     # write a string
         elif request.form.get("boton") == "derecha":
             print("derecha")
+            ser.write(b'r \n')     # write a string
         elif request.form.get("boton") == "alto":
             print("alto")
+            ser.write(b's \n')     # write a string
         else:
             print("Comando incorrecto!")
             print(request.form.get("boton"))
@@ -37,8 +44,10 @@ def commandrobot():
     if request.method == 'POST':       
         if request.form.get("boton2") == "abrir":
             print("abrir")
+            ser.write(b'abrir \n')     # write a string
         elif request.form.get("boton2") == "cerrar":
             print("cerrar")
+            ser.write(b'cerrar \n')     # write a string
         else:
             print("Comando incorrecto!")
             print(request.form.get("boton2"))
