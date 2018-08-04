1e339aaff1fd121b037185f2e02f9fec99dc928d
IvanC0110 <41236313+IvanC0110@users.noreply.github.com>
2018-08-04 12:03:38 -0300

    Responsive terminado

--- static/js/index.js
+++ static/js/index.js
@@ -1,22 +1,45 @@
 // aca empieza el codigo del preloader
-var progress = setInterval(function () {
-  var $bar = $("#bar");
 
-  if ($bar.width() >= 600) {
+var w = window.innerWidth;
+if (w > 800) {
+  var progress = setInterval(function () {
+    var $bar = $("#bar");
+
+    if ($bar.width() >= 600) {
       clearInterval(progress);
-  } else {
+    } else {
       $bar.width($bar.width() + 60);
-  }
-  $bar.text($bar.width() / 6 + "%");
-  if ($bar.width() / 6 == 100){
-    $bar.text("Still working ... " + $bar.width() / 6 + "%");
-  }
-}, 800);
+    }
+    $bar.text($bar.width() / 6 + "%");
+    if ($bar.width() / 6 == 100) {
+      $bar.text("Still working ... " + $bar.width() / 6 + "%");
+    }
+  }, 800);
 
-$(window).on('load',function() {
-$("#bar").width(600);
-$(".loader").fadeOut(3000);
-});
+  $(window).on('load', function () {
+    $("#bar").width(600);
+    $(".loader").fadeOut(3000);
+  });
+} else {
+  var progress = setInterval(function () {
+    var $bar = $("#bar");
+
+    if ($bar.width() >= 400) {
+      clearInterval(progress);
+    } else {
+      $bar.width($bar.width() + 40);
+    }
+    $bar.text($bar.width() / 4 + "%");
+    if ($bar.width() / 4 == 100) {
+      $bar.text("Still working ... " + $bar.width() / 4 + "%");
+    }
+  }, 800);
+
+  $(window).on('load', function () {
+    $("#bar").width(400);
+    $(".loader").fadeOut(3000);
+  });
+}
 
 // aca empieza el codigo de los botones
 $(function () {
