97413c56ef87e7a07d962aac598e972d78d3445e
IvanC0110 <41236313+IvanC0110@users.noreply.github.com>
2018-08-11 15:07:10 -0300

    Arduino

---
+++ CodigoArduino/CodigoArduino.ino
@@ -0,0 +1,72 @@
+const int led = 8;
+const int led2 = 9;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(led, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+void loop() {
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n' || c == '\r' || c == '$') {
+    } else {
+      control(c);
+    }
+  }
+}
+
+void control(char comando) {
+  switch (comando) {
+    case 'f': // si verde titila 2 veces es f
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, LOW);
+      delay(500);
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, LOW);
+      delay(500);
+      break;
+
+    case 'b': // si rojo titila 2 veces es b
+      digitalWrite(led2, HIGH);
+      delay(500);
+      digitalWrite(led2, LOW);
+      delay(500);
+      digitalWrite(led2, HIGH);
+      delay(500);
+      digitalWrite(led2, LOW);
+      delay(500);
+      break;
+
+    case 'l': // si rojo titila 1 vez es l
+      digitalWrite(led2, HIGH);
+      delay(500);
+      digitalWrite(led2, LOW);
+      delay(500);
+      break;
+
+    case 'r': // si verde titila 1 ves es r
+      digitalWrite(led, HIGH);
+      delay(500);
+      digitalWrite(led, LOW);
+      delay(500);
+      break;
+
+    case 's': //devuelve los dos a la vez
+    digitalWrite(led, HIGH);
+    digitalWrite(led2, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    digitalWrite(led2, LOW);
+    delay(1000);
+    break;
+  }
+
+
+
+
+}
+
+
