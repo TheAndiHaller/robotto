7af4684f53cbd82093a506a0e640b8747d23ea13
IvanC0110 <41236313+IvanC0110@users.noreply.github.com>
2018-08-18 11:02:06 -0300

    cambio de ip

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def command():
     return "OK"        
 
 if __name__ == "__main__":
-    app.run(host='172.16.0.239', port=5000)
+    app.run(host='127.0.0.1', port=5000)
 
 
 #127.0.0.1 (ip home)
